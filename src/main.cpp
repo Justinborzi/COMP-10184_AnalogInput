de1f89aa5cb3e2588263fee7e9ad9429219aeed8
Justin Borzi <justinborzi@gmail.com>
2022-09-19 16:38:20 -0400

    Add temp check and output.

--- src/main.cpp
+++ src/main.cpp
@@ -8,38 +8,39 @@ void setup()
 
 void loop()
 {
+  String tempatureString;
   // read digitized value from the D1 Mini's A/D convertor
   int iVal = analogRead(A0);
   float voltage = iVal * (3.3 / 1023.0);
-  long temp = map(voltage, 0, 3.3, 0, 50.0);
+  float temp = voltage * (50.0 / 3.3);
 
-  // print value to the USB port
-  Serial.println("Digitized Value of " + String(iVal) + " is equivalent to a tempature input of " + String(temp) + " deg. which is");
-
-  if (temp < 10)
+  if (temp <= 10)
   {
-    Serial.print("Cold!");
+    tempatureString = "Cold!";
   }
   else if (temp < 15)
   {
-    Serial.print("Cool");
+    tempatureString = "Cool";
   }
   else if (temp < 25)
   {
-    Serial.print("Perfect");
+    tempatureString = "Perfect";
   }
   else if (temp < 30)
   {
-    Serial.print("Warm");
+    tempatureString = "Warm";
   }
   else if (temp < 35)
   {
-    Serial.print("Hot");
+    tempatureString = "Hot";
   }
-  else if (temp > 35)
+  else if (temp >= 35)
   {
-    Serial.print("Too Hot!");
+    tempatureString = "Too Hot!";
   }
+
+  // print value to the USB port
+  Serial.println("Digitized Value of " + String(iVal) + " is equivalent to a tempature input of " + String(temp) + " deg. which is " + tempatureString);
 
   // wait 0.5 seconds (500 ms)
   delay(500);
